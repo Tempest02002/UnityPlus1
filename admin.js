e628585232fdb78aec414810452a000b55289a23
Y-Shivansh <sharma.shivansh1305@gmail.com>
2024-03-05 18:47:15 +0530

    transition added

--- admin.js
+++ admin.js
@@ -1,8 +1,9 @@
 $(".regis ").on('click',function(){
     var form = $(".register");
-    form.css("display", "flex");
+    form.css("display", "flex")
     $(".darkBg").css("display", "block")
     const scrollY = window.scrollY;
     document.body.style.position = 'fixed';
+    form.css("left","83.4%")
     // form.classList.add('slideInFromRight');
 })
